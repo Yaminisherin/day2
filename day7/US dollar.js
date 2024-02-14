ae938288f611075e43b16821e499eab45fc9ffab
Yaminisherin <148968723+Yaminisherin@users.noreply.github.com>
2024-02-15 01:18:12 +0530

    US dollar.js

--- day7/US dollar.js
+++ day7/US dollar.js
@@ -8,3 +8,25 @@ xhr.onload=function(){
     usDollarCountries.forEach(countries =>
         console.log(countries.name.common));
 }
+//---------------------
+output:
+Timor-Leste
+American Samoa
+El Salvador
+Guam
+Puerto Rico
+Panama
+British Virgin Islands
+British Indian Ocean Territory
+United States
+Cambodia
+Bahamas
+United States Virgin Islands
+Marshall Islands
+United States Minor Outlying Islands
+Northern Mariana Islands
+Micronesia
+Turks and Caicos Islands
+Ecuador
+Palau
+Caribbean Netherlands
