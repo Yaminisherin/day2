69f722164ffc0cca37384907d19de1ac307977a9
Yaminisherin <148968723+Yaminisherin@users.noreply.github.com>
2024-02-15 01:20:19 +0530

    Update asiacontinent.js

--- day7/asiacontinent.js
+++ day7/asiacontinent.js
@@ -9,3 +9,55 @@ xhr.onload=function(){
         console.log(countries.name.common);
     });
     }
+//---------------------------------------
+output:
+Turkmenistan
+Timor-Leste
+Azerbaijan
+Vietnam
+Afghanistan
+Kuwait
+Kyrgyzstan
+Armenia
+Tajikistan
+Brunei
+Pakistan
+Bangladesh
+China
+Maldives
+Malaysia
+Lebanon
+Mongolia
+Nepal
+Taiwan
+Bhutan
+Cambodia
+Georgia
+Israel
+Singapore
+United Arab Emirates
+Uzbekistan
+Saudi Arabia
+Japan
+Syria
+Iran
+South Korea
+Philippines
+Macau
+North Korea
+Iraq
+Palestine
+Turkey
+Kazakhstan
+Qatar
+Jordan
+Myanmar
+Thailand
+Bahrain
+Laos
+India
+Yemen
+Oman
+Indonesia
+Sri Lanka
+Hong Kong
